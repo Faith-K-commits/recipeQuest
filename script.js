42cbc0265d48428d09e92760e4865f2925908305
Faith Kariuki <tcwamboi@gmail.com>
2024-07-19 14:17:44 +0300

    Toggle random section display with search results

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const apiKey = '0a994f055a9e491ab6fc160fac836c64';
 
 document.addEventListener('DOMContentLoaded', () => {
+    const randomSection = document.getElementById('random-section');
+    const resultsSection = document.getElementById('results-section');
+    const inputElement = document.getElementById('ingredient-input');
+
     fetch(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=8`)
         .then(response => response.json())
         .then(data => {
             const randomRecipes = data.recipes;
-            const randomSection = document.getElementById('random-section');
             randomSection.innerHTML = ''; 
 
             const gridContainer = document.createElement('div');
@@ -40,54 +43,54 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('search-button').addEventListener('click', searchRecipes);
 
-    document.getElementById('ingredient-input').addEventListener('keydown', event => {
+    inputElement.addEventListener('keydown', event => {
         if (event.key === 'Enter') {
             event.preventDefault(); // Prevent the default form submission behavior
             searchRecipes();
-            document.getElementById('ingredient-input').value = '';
         }
     });
 
     function searchRecipes() {
-        const inputElement = document.getElementById('ingredient-input');
-        const input = inputElement.value;
-        const ingredients = input.split(',').map(ingredient => ingredient.trim());
-        const queryString = ingredients.join(',');
+        const input = inputElement.value.trim();
+
+        randomSection.style.display = 'none'; // Hide the random recipes section
+        resultsSection.innerHTML = ''; 
 
         if (input === '') {
             displayMessage('Please enter an ingredient to search for recipes.');
             return;
         }
+
+        const ingredients = input.split(',').map(ingredient => ingredient.trim());
+        const queryString = ingredients.join(',');
+
         fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${queryString}&apiKey=${apiKey}`)
             .then(response => response.json())
             .then(recipes => {
                 if (recipes.length === 0) {
                     displayMessage('No recipes found for the given ingredients.');
                 } else {
-                    const resultsSection = document.getElementById('results-section');
-                    resultsSection.innerHTML = ''; // Clear previous results
-    
                     const heading = document.createElement('h2');
                     heading.textContent = 'Your search results:';
                     heading.classList.add('results-heading');
                     resultsSection.appendChild(heading);
-    
+
                     const gridContainer = document.createElement('div');
                     gridContainer.classList.add('grid-container');
                     resultsSection.appendChild(gridContainer);
-    
+
                     recipes.forEach(recipe => {
                         const card = document.createElement('div');
                         card.classList.add('recipe-card');
-    
+
                         const img = document.createElement('img');
                         img.src = recipe.image;
                         card.appendChild(img);
-    
+
                         const title = document.createElement('h3');
                         title.textContent = recipe.title;
                         card.appendChild(title);
-    
+
                         const instructionsButton = document.createElement('button');
                         instructionsButton.textContent = 'View Instructions';
                         instructionsButton.addEventListener('click', () => {
@@ -95,19 +98,18 @@ document.addEventListener('DOMContentLoaded', () => {
                             resultsSection.style.display = 'none';
                         });
                         card.appendChild(instructionsButton);
-    
+
                         gridContainer.appendChild(card);
                     });
                 }
-                
-    
-                // Clear the input field after displaying the results
-                inputElement.value = '';
+
+                inputElement.value = ''; // Clear the input field after displaying the results
             })
             .catch(error => {
                 console.error('Error fetching recipes:', error);
             });
     }
+
 
     function displayInstructions(recipeId) {
         fetch(`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`)
